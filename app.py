8b9de6eebbded23b33caf5da724655c99afbaf18
m p <mtpap@MacBook-Pro.local>
2023-10-23 23:55:29 -0400

    added the first 4 routes

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
+# import flask
+from flask import Flask
 
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
+# set up app variable
+app = Flask(__name__)
+
+# First route
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return 'Are you there, world? It\'s me, Ducky!'
+
+# Second route
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    """Display a message to the user that changes based on their favorite animal."""
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+# Third route (dessert)
+@app.route('/dessert/<users_dessert>')
+def favorite_dessert(users_dessert):
+    """Display a message to the user that changes based on their favorite dessert."""
+    return f'How did you know I liked {users_dessert}?'
+
+# Fourth route (madlibs)
+@app.route('/madlibs/<adjective>/<noun>')
+def make_madlib(adjective, noun):
+    """Display a madlib story based on the user's input adjective and noun"""
+    return f'There once was a dog named Spot. His favorite toy was a {noun}. He loved his {adjective} {noun}.'
+
+# run the server
+if __name__ == '__main__':
+    app.run(debug=True)
